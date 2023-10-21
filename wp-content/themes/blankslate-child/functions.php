7154f52623a497686fabbc3478776f4e126856e4
Valeriane <valerianecaubet@gmail.com>
2023-10-21 07:14:05 +0200

    creation page commande et ajout css

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -19,6 +19,13 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
         wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+}
 
 // END ENQUEUE PARENT ACTION
+
+
